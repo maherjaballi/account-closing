c2c98899d560e1f68dc4829cc7d688b9c16b1828
Maher <maher@tenovar.com>
2019-11-19 14:04:18 +0100

    update 19-11-2019 branch 12.0

--- account-closing-config/models/res_config.py
+++ account-closing-config/models/res_config.py
@@ -5,7 +5,7 @@ from openerp import api, fields, models, _
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
     
-    module_account_cutoff_base = fields.Boolean(string='account cutoff base',
+    module_account_cutoff_base = fields.Boolean(string='Account Cutoff Base',
         help='This module contains models, fields and menu entries that are used by \n'
               'other cut-off modules ; it doesnt provide useful features by itself. You \n'
               'need to install other cut-off modules to get the useful features: \n'
@@ -15,12 +15,12 @@ class ResConfigSettings(models.TransientModel):
                ' on the status of the pickings. \n'
              '-This installs the module account_cutoff_base.')
     
-    module_account_cutoff_prepaid = fields.Boolean(string='account_cutoff_prepaid',
+    module_account_cutoff_prepaid = fields.Boolean(string='Account Cutoff Prepaid',
         help='This module allows you to easily compute the prepaid revenue and prepaid expenses and to generate \n'
                'the related cutoff journal items. It uses the **Start Date** and **End Date** fields of journal entries \n' 
                '(copied from the same fields of invoice lines). \n')
 
-    module_account_invoice_start_end_dates = fields.Boolean(string='account invoice start end dates',
+    module_account_invoice_start_end_dates = fields.Boolean(string='Account Invoice Start End Dates',
         help='This module adds the fields *Start Date* and *End Date* on invoice lines. When you validate the invoice,  \n'
               'the information is copied from invoice lines to account move lines (if you enabled the grouping option on the related journal,  \n'
               'Odoo will not group invoice lines that have different start/end dates). \n'
@@ -35,7 +35,7 @@ class ResConfigSettings(models.TransientModel):
              'the module *account_cutoff_prepaid* '
               '(same repository): this module allows easy computation of prepaid expenses and prepaid revenues. \n')
     
-    module_account_multicurrency_revaluation = fields.Boolean(string='account multicurrency revaluation',
+    module_account_multicurrency_revaluation = fields.Boolean(string='Account Multicurrency Revaluation',
         help='This module was written to extend the functionality of the accounting module to \n'
               'support the multicurrency and to allow you to generate automatically \n'
               'revaluation journal entries. \n'
